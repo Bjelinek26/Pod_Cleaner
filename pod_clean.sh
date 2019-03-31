0323772471d5da622541d4aea5349492fb735736
Bjelinek26 <brad.jelinek.226@gmail.com>
2019-03-31 15:33:30 -0400

    parsing contents of directory and applying filter only to files not directories

--- pod_clean.sh
+++ pod_clean.sh
@@ -9,29 +9,46 @@ cd ~/Podcasts/TWIT/
 # to do: add all directories to an array
 # for each loop mv to correct dir based on regex
 # to do: add option to added new directory 
-# and add this to the array
+# and add this new directory to the array
 
+n=1
+z=1    
+while read FILES
+do  
+  if [[ -d $FILES ]]
+  then 
+    echo "i am dir number ${n}"
+    ((n++))
+  else [[ -f $FILES ]]
+    echo "i am a file numer ${z}" 
+    STUFF=$FILES
+    for STUFF in $STUFF
+    do 
+      mv *android* All_About_Android/ 2>/dev/null
 
-mv *android* All_About_Android/
+      mv *floss* FLOSS/ 2>/dev/null
 
-mv *floss* FLOSS/
+      mv *ham* HAM/ 2>/dev/null
 
-mv *ham* HAM/
+      mv *ios* IOS/ 2>/dev/null
 
-mv *ios* IOS/
+      mv *mac* Macbreak_Weekly 2>/dev/null
 
-mv *mac* Macbreak_Weekly
+      mv *guy* The_Tech_Guy 2>/dev/null
 
-mv *guy* The_Tech_Guy
+      mv *week* This_Week_In_Tech/ 2>/dev/null
 
-mv *week* This_Week_In_Tech/
+      mv *hardware* TWICH/ 2>/dev/null
+ 
+      mv *google* TWIG/ 2>/dev/null
 
-mv *hardware* TWICH/
+      mv *specials* TWIT_Specials/ 2>/dev/null
 
-mv *google* TWIG/
+      mv *windows* Windows_Weekly/ 2>/dev/null
 
-mv *specials* TWIT_Specials/
+      mv *security* Security_Now/FILES 2>/dev/null 
+    done 
+   ((z++))     
+  fi
+done <<< $(ls)
 
-mv *windows* Windows_Weekly/
-
-mv *security* Security_Now/ 
